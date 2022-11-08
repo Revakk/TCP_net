5ee53464c7ddd207532650964d36ed6e8181f0d7
Revakhoron <Sevcik.jan42@seznam.cz>
2022-11-08 21:00:44 +0100

    client template interface

--- tcp_server/tcp_session.hpp
+++ tcp_server/tcp_session.hpp
@@ -7,18 +7,35 @@
 namespace net
 {
 	template<typename T>
-	class connection : public std::enable_shared_from_this<connection<T>>
+	class session : public std::enable_shared_from_this<session<T>>
 	{
 	public:
-		connection()
+		session()
 		{
 
 		}
 
-		virtual ~connection()
+		virtual ~session()
 		{
 
 		}
+
+	public:
+		bool connect_to_sever();
+		bool disconnect();
+		bool is_connected() const;
+
+	public:
+		bool send_message(const message<T>& _msg);
+
+	protected:
+		boost::asio::ip::tcp::socket socket_;
+
+		boost::asio::io_context& asio_context_;
+
+		message_queue<message<T>> q_msgs_out_;
+
+		message_queue<owned_message<T>>& q_messages_in_;
 
 	private:
 
